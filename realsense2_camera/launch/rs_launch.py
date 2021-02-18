fcfb6e3c11a184e555c872541d31005409cb9885
charlielito <candres.alv@gmail.com>
2021-02-18 20:42:15 +0000

    [FEAT]: Unite accel and gyro by default to one imu topic

--- realsense2_camera/launch/rs_launch.py
+++ realsense2_camera/launch/rs_launch.py
@@ -36,7 +36,7 @@ configurable_parameters = [{'name': 'camera_name',                  'default': '
                            {'name': 'device_type',                  'default': '', 'description': 'choose device by type'},
                            {'name': 'config_file',                  'default': '', 'description': 'yaml config file'},
                            {'name': 'enable_pointcloud',            'default': 'true', 'description': 'enable pointcloud'},
-                           {'name': 'unite_imu_method',             'default': '', 'description': '[copy|linear_interpolation]'},                           
+                           {'name': 'unite_imu_method',             'default': 'copy', 'description': '[copy|linear_interpolation]'},                           
                            {'name': 'json_file_path',               'default': '', 'description': 'allows advanced configuration'},                           
                            {'name': 'output',                       'default': 'screen', 'description': 'pipe node output [screen|log]'},                           
                            {'name': 'depth_width',                  'default': os.getenv('STEREO_DEPTH_WIDTH','640'), 'description': 'depth image width'},                           
