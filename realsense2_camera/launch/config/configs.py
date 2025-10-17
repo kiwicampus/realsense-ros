a4fb2ff08a77361513d5ceeb618358c336e67c8b
Juanda <advancedrobotics@robot.com>
2025-10-17 08:04:10 +0800

    realsense only for wrists

--- realsense2_camera/launch/config/configs.py
+++ realsense2_camera/launch/config/configs.py
@@ -5,16 +5,16 @@ This file contains the configurations for the realsense cameras.
 configs = {
     "bimanual_i2rt": {
         "cameras": [
-            {"name": "main_camera", "serial": "017322071831"},      # Only use the working D435
-            {"name": "right_wrist_camera", "serial": "216322072395"}, # D435I has hardware issues - disabled
-            {"name": "left_wrist_camera", "serial": "1234567890"},
+            # {"name": "main_camera", "serial": "017322071831"},      # Only use the working D435
+            {"name": "right_wrist_camera", "serial": "151222079231"}, # D435I has hardware issues - disabled
+            {"name": "left_wrist_camera", "serial": "151222078659"},
         ]
     },
     "default": {
         "cameras": [
             {"name": "main_camera", "serial": "017322071831"},      # Only use the working D435  
-            {"name": "right_wrist_camera", "serial": "216322072395"}, # D435I has hardware issues - disabled
-            {"name": "left_wrist_camera", "serial": "1234567890"},
+            {"name": "right_wrist_camera", "serial": "151222078659"}, # D435I has hardware issues - disabled
+            {"name": "left_wrist_camera", "serial": "151222079231"},
         ]
     }
 }
